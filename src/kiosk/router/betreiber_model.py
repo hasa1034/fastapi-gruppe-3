253bfc04d7b53daee5b95f7a5de72d453d79252e
SAMHKA <hasa1034@h-ka.de>
2026-04-11 21:44:35 +0200

    update

--- src/kiosk/router/betreiber_model.py
+++ src/kiosk/router/betreiber_model.py
@@ -14,6 +14,7 @@
 # along with this program.  If not, see <https://www.gnu.org/licenses/>.
 
 """Pydantic-Model für die Betreiber."""
+
 from typing import Annotated
 
 from pydantic import BaseModel, ConfigDict, StringConstraints
